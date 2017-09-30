2085f190f02a421d10da0b3c1a0a27f3e084dfa4
Administrator <Administrator@SC-201706212248>
2017-09-30 17:28:53 +0800

    。

---
+++ src/test/java/JfinalPlugin.java
@@ -0,0 +1,42 @@
+
+
+/**
+ * 
+ */
+
+/**
+ * @author zengjintao
+ * @version 1.0
+ * @create_at 2017年9月30日下午5:26:43
+ */
+public class JfinalPlugin {
+
+//	public static void startPlugins(Plugins plugins) {
+//		List<IPlugin> pluginList = plugins.getPluginList();
+//		if (pluginList == null) {
+//			return ;
+//		}
+//		for (IPlugin plugin : pluginList) {
+//			try {
+//				if (plugin instanceof com.jfinal.plugin.activerecord.ActiveRecordPlugin) {
+//					com.jfinal.plugin.activerecord.ActiveRecordPlugin arp = (com.jfinal.plugin.activerecord.ActiveRecordPlugin)plugin;
+//					if (arp.getDevMode() == null) {
+//						
+//					}
+//				}
+//				if (plugin.start() == false) {
+//					String message = "Plugin start error: " + plugin.getClass().getName();
+//					throw new RuntimeException(message);
+//				}
+//			}
+//			catch (Exception e) {
+//				String message = "Plugin start error: " + plugin.getClass().getName() + ". \n" + e.getMessage();
+//				throw new RuntimeException(message, e);
+//			}
+//		}
+//	}
+//	
+//	public static void initLogFactory() {
+//		LogManager.me().init();
+//	}
+}
--- src/test/java/com/springboot/test/DemoApplicationTests.java
+++ src/test/java/com/springboot/test/DemoApplicationTests.java
@@ -1,39 +1,52 @@
 package com.springboot.test;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.springboot.entity.SystemAdmin;
-import com.springboot.jpa.SystemAdminPage;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
 	
-	@Autowired
-	private SystemAdminPage systemAdminPage;
-	
-	@Test
-	public void contextLoads() {
-		Order order = new Order(Direction.DESC, "id");
-		Sort sort = new Sort(order);
-		//0代表第一页
-		Page<SystemAdmin> page = systemAdminPage.findAll(new PageRequest(0, 30,sort));
-		System.out.println(page.getTotalPages()+1);
-		System.out.println(page.getContent());
-	}
-
-	@Test
-	public void test(){
-		
-	}
+//	@Autowired
+//	private SystemAdminPage systemAdminPage;
+//	
+//	@Test
+//	public void contextLoads() {
+//		Order order = new Order(Direction.DESC, "id");
+//		Sort sort = new Sort(order);
+//		//0代表第一页
+//		Page<SystemAdmin> page = systemAdminPage.findAll(new PageRequest(0, 30,sort));
+//		System.out.println(page.getTotalPages()+1);
+//		System.out.println(page.getContent());
+//	}
+//
+//	@Test
+//	public void test(){
+//		private static final String url = "jdbc:mysql://127.0.0.1/furniture?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
+//		
+//		private static final String url1 = "jdbc:mysql://127.0.0.1/alone?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
+//		@SuppressWarnings("unused")
+//		@Test
+//		public  void jdbcTest() {
+//			JfinalPlugin.initLogFactory();
+//		    Plugins plugins = new Plugins();
+//		    DruidPlugin druid = new DruidPlugin(url, "root","123456");
+//		    plugins.add(druid);
+//			ActiveRecordPlugin activeRecordPlugin = new ActiveRecordPlugin("furniture",druid);
+//			activeRecordPlugin.addMapping("goods_info", GoodsInfo.class);
+//			plugins.add(activeRecordPlugin);
+//			
+//			DruidPlugin druid1 = new DruidPlugin(url1, "root","123456");
+//		    plugins.add(druid1);
+//			ActiveRecordPlugin activeRecordPlugin1 = new ActiveRecordPlugin("alone",druid1);
+//			activeRecordPlugin.addMapping("goods_info", GoodsInfo.class);
+//			plugins.add(activeRecordPlugin);
+//			JfinalPlugin.startPlugins(plugins);
+//			Record goodsInfo =Db.use("alone").findFirst("select * from goods_info where id=1");;
+//			System.out.println(goodsInfo);
+//		}
+//	}
 }
